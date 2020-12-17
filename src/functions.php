d84077ee50d1d066f704c50774e450c518cfb2ff
ludvig <ludvigrandersen@gmail.com>
2020-12-17 11:19:40 +0100

    changes to api description

--- src/functions.php
+++ src/functions.php
@@ -1,4 +1,12 @@
 <?php
+
+  function formatError(){
+    $output['error'] = 'Incorrect URL format';
+    $output['message'] = 'Correct format: {host}/notes-api/v1/';
+
+    return json_encode($output);
+  }
+
   function APIDescription(){
     $baseUrl = "{host}/notes-api/v1/";
 
@@ -6,12 +14,14 @@
 
     $apiDescription['artist'] = [
       ['about' => 'Get all artists', 'method' => 'GET', 'url' => $baseUrl . 'artist'],
+      ['about' => 'Get all artists based on page # - Max 25 records pr. page', 'method' => 'GET', 'url' => $baseUrl . 'artist?p={page}'],
       ['about' => 'Create new artist', 'method' => 'POST', 'url' => $baseUrl . 'artist', 'request-body' => ["name" => '']],
       ['about' => 'Update artist', 'method' => 'PUT', 'url' => $baseUrl . 'artist/:id', 'request-body' => ["name" => '']],
       ['about' => 'Delete artist', 'method' => 'DELETE', 'url' => $baseUrl . 'artist/:id']
     ];
 
     $apiDescription['album'] = [
+      ['about' => 'Get all albums', 'method' => 'GET', 'url' => $baseUrl . 'album'],
       ['about' => 'Get all albums based on page # - Max 25 records pr. page', 'method' => 'GET', 'url' => $baseUrl . 'album?p={page}'],
       ['about' => 'Create new album', 'method' => 'POST', 'url' => $baseUrl . 'album', 'request-body' => ["name" => '', "artistId" => '']],
       ['about' => 'Update album', 'method' => 'POST', 'url' => $baseUrl . 'album/:id', 'request-body' => ["name" => '']],
@@ -30,7 +40,10 @@
     ];
 
     $apiDescription['invoice'] = [
-
+      ['about' => 'Create new invoice', 'method' => 'POST', 'url' => $baseUrl . 'invoice', 'request-body' => [
+        'customerId' => '', 'billingAddress' => '', 'billingState' => '', 'billingCountry' => '', 'billingPostalCode' => '', 'total' => '',
+        'items' => [array('trackId' => '', 'price' => '', 'quantity' => '')]
+      ]]
     ];
 
     return json_encode($apiDescription);
